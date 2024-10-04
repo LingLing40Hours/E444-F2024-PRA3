cf834fb546782366b790807da15b882868b300f5
Xiangyu Chen <chenxy327@gmail.com>
2024-10-03 22:11:01 -0400

    fix no such table:post error yet again

--- flaskr-tdd/project/app.py
+++ flaskr-tdd/project/app.py
@@ -85,4 +85,6 @@ def delete_entry(post_id):
 
 
 if __name__ == "__main__":
+    #with app.app_context():
+    #    db.create_all()  # This will create all tables defined in models.py
     app.run()
